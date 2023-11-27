74a19649f6068e6d38819409a351bae608493bce
Gnosis <emmagab38@gmail.com>
2023-11-27 14:44:42 +0100

    commit changes

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
 {
     public function users()
     {
-        $users = User::all();
+        $users = User::where('admin', 0)->get();
 
         return view('admin.user.list', compact('users'));
     }
